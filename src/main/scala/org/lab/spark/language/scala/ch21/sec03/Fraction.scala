726c65de15cd36c51a61e35b026d76b6022f30b9
ywang <yong.wang@ucarinc.com>
2017-12-09 20:10:35 +0800

    隐式函数的转化

---
+++ src/main/scala/org/lab/spark/language/scala/ch21/sec03/Fraction.scala
@@ -0,0 +1,46 @@
+package org.lab.spark.language.scala.ch21.sec03
+
+import scala.math._
+
+/**
+  * Created by wy on 12/9/17.
+  */
+class Fraction(n: Int, d: Int) {
+
+  val num: Int = if (d == 0) 1 else n * sign(d) / gcd(n, d)
+
+  val den: Int = if (d == 0) 0 else d * sign(d) / gcd(n, d)
+
+  override def toString: String = num + "/" + den
+
+  def sign(a: Int) = if (a > 0) 1 else if (a < 0) -1 else 0
+
+  def gcd(a: Int, b: Int): Int = if (b == 0) abs(a) else gcd(b, a % b)
+
+  def *(other: Fraction) = new Fraction(num * other.num, den * other.den)
+}
+
+object Fraction {
+
+  def apply(n: Int, d: Int): Fraction = new Fraction(n, d)
+}
+
+package com.horstmann.impatient {
+
+  object FractionConversions {
+
+    implicit def int2Fraction(n: Int) = Fraction(n, 1)
+
+    implicit def fraction2Double(f: Fraction) = f.num * 1.0 / f.den
+  }
+
+}
+
+object Main extends App {
+
+  import com.horstmann.impatient.FractionConversions._
+
+  val result = 3 * Fraction(4, 5)
+
+  println(result)
+}
