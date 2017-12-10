0ee555d52447a400a7cc5050b97995a95ef54898
ywang <yong.wang@ucarinc.com>
2017-12-10 12:28:16 +0800

    基础知识的数据补充

---
+++ src/main/scala/org/lab/spark/language/scala/ch01/sec02/repl-session.scala
@@ -0,0 +1,18 @@
+// These are meant to be typed into the REPL. You can also run
+// scala -Xnojline < repl-session.scala to run them all at once.
+
+val answer = 8 * 5 + 2
+
+0.5 * answer
+
+val greeting: String = null
+
+answer = 0 //This will give an error
+
+var counter = 0
+
+counter = 1 //OK , can change a var
+
+var i, j = 0
+
+var greeting2, message: String = null
---
+++ src/main/scala/org/lab/spark/language/scala/ch01/sec04/repl-session.scala
@@ -0,0 +1,10 @@
+// These are meant to be typed into the REPL. You can also run  
+// scala -Xnojline < repl-session.scala to run them all at once.
+
+"Hello" intersect "World"
+
+var counter = 0
+
+val x: BigInt = 1234567890
+
+x * x * x
